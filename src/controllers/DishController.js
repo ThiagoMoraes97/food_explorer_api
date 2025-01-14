759fbc307885f96b230766679b240e89d4c916eb
Thiago Moraes <thiagomottamoraes@gmail.com>
2025-01-13 23:44:55 -0300

    Completed the create, update, show, and delete methods of the dishes controller

--- src/controllers/DishController.js
+++ src/controllers/DishController.js
@@ -1,20 +1,40 @@
 const knex = require("../database/knex");
+const DiskStorage = require("../provider/DiskStorage");
 class DishController {
     
     async create(request, response) {
         const { name, description, price, category, ingredients } = request.body;
+        const dishImage = request.file;
         const user_id = request.user.id;
 
-        const [dish_id] = await knex("dishes").insert({
-            name,
-            description,
-            price,
-            category,
-            user_id
-        });
+       let dish_id;
 
+        const diskStorage = new DiskStorage();
+        
+        if(dishImage){
+            const filename = await diskStorage.saveFile(dishImage.filename);
+
+            [dish_id] = await knex("dishes").insert({
+                name,
+                description,
+                price,
+                category,
+                image: filename,
+                user_id
+            });
+        } else {
+            [dish_id] = await knex("dishes").insert({
+                name,
+                description,
+                price,
+                category,
+                user_id
+            });
+    
+        }
+      
         if(ingredients){
-            const dishIngredients = ingredients.map( ingredient => {
+            const dishIngredients = JSON.parse(ingredients).map( ingredient => {
                 return {
                     name: ingredient,
                     dish_id
@@ -28,7 +48,81 @@ class DishController {
         
     }
 
+    async update(request, response) {
+        const { name, description, price, category, ingredients } = request.body;
+        const dishImage = request.file;
+        const { id } = request.params;
 
+        const dish = await knex("dishes").where({ id }).first();
+
+        if(!dish) {
+            throw new AppError("Prato não encontrado");
+        }
+
+        dish.name = name ?? dish.name;
+        dish.description = description ?? dish.description;
+        dish.price = price ?? dish.price;
+        dish.category = category ?? dish.category;
+
+        const diskStorage = new DiskStorage();
+
+        if(dish.image) {
+            await diskStorage.deleteFile(dish.image);
+        }
+        
+        if(dishImage){
+            const filename = await diskStorage.saveFile(dishImage.filename);
+
+            dish.image = filename;
+
+            await knex("dishes").update(dish).where({ id });
+        } else {
+            await knex("dishes").update(dish).where({ id });
+    
+        }
+      
+        if(ingredients){
+            const dishIngredients = JSON.parse(ingredients).map( ingredient => {
+                return {
+                    name: ingredient,
+                    dish_id: dish.id
+                }
+            })
+
+            await knex("ingredients").where({ dish_id: dish.id }).delete();
+
+            await knex("ingredients").insert(dishIngredients);
+        }
+
+        return response.json();
+    }
+
+    async show(request, response) {
+        const { id } = request.params;
+
+        const dish = await knex("dishes").where({ id }).first();
+
+        if(!dish) {
+            throw new AppError("Prato não encontrado");
+        }
+
+        const ingredients = await knex("ingredients").where({ dish_id: id }).orderBy("name");
+
+        const dishWithIngredients = {
+            ...dish,
+            ingredients
+        }
+
+        return response.json(dishWithIngredients);
+    }
+
+    async delete(request, response) {
+        const { id } = request.params;
+
+        await knex("dishes").where({ id }).delete();
+
+        return response.json();
+    }
 
 };
 
--- src/routes/dish.routes.js
+++ src/routes/dish.routes.js
@@ -2,12 +2,17 @@ const { Router } = require("express");
 const DishController = require("../controllers/DishController");
 const ensureAuthenticated = require("../middlewares/ensureAuthenticated");
 const verifyUserAuthorization = require("../middlewares/verifyUserAuthorization");
+const uploadConfig = require("../configs/upload");
+const multer = require("multer");
 
 dishRoutes = Router();
 
 const dishController = new DishController();
 
-dishRoutes.post("/", ensureAuthenticated, dishController.create);
+dishRoutes.post("/", ensureAuthenticated, multer(uploadConfig.MULTER).single("image"), dishController.create);
+dishRoutes.put("/:id", ensureAuthenticated, multer(uploadConfig.MULTER).single("image"), dishController.update);
+dishRoutes.get("/:id", ensureAuthenticated, dishController.show);
+dishRoutes.delete("/:id", ensureAuthenticated, dishController.delete);
 
 
 module.exports = dishRoutes;
